822b466afd1cff786717634b635534bb848e7472
SamuelBFG <samuelbfgomes@gmail.com>
2020-09-10 16:59:58 -0300

    fmnist 99.8acc

--- Fashion MNIST/model_with_callbacks.py
+++ Fashion MNIST/model_with_callbacks.py
@@ -31,17 +31,18 @@ def train_mnist():
     print('Y_test.shape', y_test.shape)
 
     # Transforming the inputs to 4D tensor:
-    x_train = x_train.reshape(x_train.shape[0],x_train.shape[1],x_train.shape[2], 1)
-    x_test = x_test.reshape(x_test[0],x_test[1],x_test[2], 1)
+    x_train = x_train.reshape(60000, 28, 28, 1)
+    x_test = x_test.reshape(10000, 28, 28, 1)
 
     # Model
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(64, (5, 5), activation='relu', input_shape=(28,28,1)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28,28,1)),
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
         tf.keras.layers.MaxPooling2D(2,2),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(12, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dense(10, activation='softmax')
-        
     ])
 
     # Compile
@@ -49,6 +50,7 @@ def train_mnist():
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
     
+    print(model.summary())
 
     # Fit
     history = model.fit(x_train, y_train, epochs=15, callbacks=[callbacks])
